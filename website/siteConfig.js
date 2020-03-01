6ca6337c309fab48cbed1ea80e3494067b5bc8b9
Sean Boult <hacksore@mcskinsearch.com>
2020-03-01 10:14:32 -0600

    RTFM

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'bluelinky', // Title for your website.
   tagline: 'The Hyundai BlueLink API Library',
   url: 'https://hacksore.github.io',
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/bluelinky-docs', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
