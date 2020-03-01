2ad7ade790fed4a4f76a153cf1fbaba0343bc795
Sean Boult <hacksore@mcskinsearch.com>
2020-03-01 10:13:10 -0600

    Fix config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'bluelinky', // Title for your website.
   tagline: 'The Hyundai BlueLink API Library',
-  url: 'https://hacksore.github.io/bluelinky',
+  url: 'https://hacksore.github.io',
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
