f4f427c103a7fae47d4697e26238a23833f35d1f
Sean Boult <hacksore@mcskinsearch.com>
2020-03-01 09:41:20 -0600

    Fix site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'bluelinky', // Title for your website.
   tagline: 'The Hyundai BlueLink API Library',
-  url: 'https://hacksore.github.io/bluelinky/',
+  url: 'https://hacksore.github.io/bluelinky',
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,10 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'bluelinky-docs',
-  organizationName: 'Hacksore',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+  organizationName: 'hacksore',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
